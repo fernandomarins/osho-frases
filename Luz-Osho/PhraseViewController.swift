49c9d180208c49f094b1982237b2072c540a410e
Fernando Marins <f.augustomarins@gmail.com>
2017-05-09 11:04:46 -0300

    Added Firebase to donwload phrases

---
+++ Luz-Osho/PhraseViewController.swift
@@ -0,0 +1,25 @@
+//
+//  PhraseViewController.swift
+//  Luz-Osho
+//
+//  Created by Fernando Augusto de Marins on 09/05/17.
+//  Copyright © 2017 Fernando Augusto de Marins. All rights reserved.
+//
+
+import UIKit
+
+class PhraseViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Pods/Target Support Files/Pods-Luz-Osho/Pods-Luz-Osho-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Luz_Osho : NSObject
+@end
+@implementation PodsDummy_Pods_Luz_Osho
+@end
